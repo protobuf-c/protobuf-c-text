4e93f4731f4520beba90e6af4b6da989c015585f
Kevin Lyda <kevin@ie.suberic.net>
2014-03-22 19:01:58 +0000

    Actually test...

--- t/test_generation.sh
+++ t/test_generation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 test -f t/moo.ab || ./t/add_person t/moo.ab
-./t/c-dump t/moo.ab
-./t/dump_people t/moo.ab
+./t/c-dump t/moo.ab > t/moo.c.text
+./t/dump_people t/moo.ab > t/moo.c++.text
+
+cmp t/moo.c.text t/moo.c++.text
