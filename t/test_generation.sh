53fb9f943b949b29081dcdfb67c74fdc2f937857
Kevin Lyda <kevin@ie.suberic.net>
2014-03-24 19:01:52 +0000

    Remove dump_people.
    User the protoc-c --decode option instead.

--- t/test_generation.sh
+++ t/test_generation.sh
@@ -2,6 +2,7 @@
 
 test -f t/moo.ab || ./t/add_person t/moo.ab
 ./t/c-dump t/moo.ab > t/moo.c.text
-./t/dump_people t/moo.ab > t/moo.c++.text
+protoc-c --decode=tutorial.AddressBook addressbook.proto \
+  < t/moo.ab > t/moo.c++.text
 
 cmp t/moo.c.text t/moo.c++.text
