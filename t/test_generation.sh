5ddcf5e34926e05a4b94dd6f18df803e99a5c3b5
Kevin Lyda <kevin@ie.suberic.net>
2014-03-29 22:46:48 +0000

    More build clean up.
    Rename parser test script. Fix coverage build.  Rename generator
    output for protoc-c.

--- t/test_generation.sh
+++ t/test_generation.sh
@@ -2,6 +2,6 @@
 
 ./t/c-dump t/addressbook.data > t/addressbook.c.text
 protoc-c --decode=tutorial.AddressBook t/addressbook.proto \
-  < t/addressbook.data > t/addressbook.c++.text
+  < t/addressbook.data > t/addressbook.protoc-c.text
 
-cmp t/addressbook.c.text t/addressbook.c++.text
+cmp t/addressbook.c.text t/addressbook.protoc-c.text
--- t/test_parse.sh
+++ t/test_parse.sh
@@ -3,6 +3,6 @@
 if [[ ! -f t/addressbook.c.text ]]; then
   exit 77
 fi
-./t/c-intake t/addressbook.c.data < t/addressbook.c.text
+./t/c-parse t/addressbook.c.data < t/addressbook.c.text
 
 cmp t/addressbook.c.data t/addressbook.data
